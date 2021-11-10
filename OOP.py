529ddb8d2166ee2c005c5824a505ccb6d10dfb5a
Murkyside <49945541+Murkyside@users.noreply.github.com>
2021-11-10 14:15:11 +0000

    Initial commit

---
+++ OOP.py
@@ -0,0 +1,18 @@
+class Example:
+  classVariable = 52
+
+  def __init__(self,myInput):
+    self.objectVariable = myInput
+
+  
+  def classMethod(self,number):
+    print(self.objectVariable + number)
+
+anExample1 = Example(76)
+anExample2 = Example(22)
+
+print(anExample1.objectVariable, anExample2.objectVariable)
+print(anExample1.classVariable, anExample2.classVariable)
+
+anExample1.classMethod(3)
+anExample2.classMethod(4)
---
+++ main.py
@@ -0,0 +1,7 @@
+import json
+
+
+
+#This is the central part of our project
+def main():
+  print('hello world')
